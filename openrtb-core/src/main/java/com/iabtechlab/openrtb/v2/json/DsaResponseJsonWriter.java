3c16d2acdafd92e6d0eddd6b5bbcde0689d158c8
mprykhodko <mprykhodko@magnite.com>
2024-02-08 19:00:50 +0200

    add DsaResponseJsonReader and DsaResponseJsonWriter

---
+++ openrtb-core/src/main/java/com/iabtechlab/openrtb/v2/json/DsaResponseJsonWriter.java
@@ -0,0 +1,39 @@
+package com.iabtechlab.openrtb.v2.json;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.iabtechlab.openrtb.v2.OpenRtbExt;
+
+public class DsaResponseJsonWriter {
+
+    private final TransparencyJsonWriter transparencyJsonWriter;
+
+    public DsaResponseJsonWriter() {
+        this.transparencyJsonWriter = new TransparencyJsonWriter();
+    }
+
+    public void write(OpenRtbExt.DsaResponse dsaResponse, JsonGenerator jsonGenerator) throws IOException {
+        jsonGenerator.writeStartObject();
+
+        writeDsaResponseField(dsaResponse, jsonGenerator);
+
+        jsonGenerator.writeEndObject();
+    }
+
+    private void writeDsaResponseField(OpenRtbExt.DsaResponse dsaResponse, JsonGenerator jsonGenerator) throws IOException {
+        if (dsaResponse.hasBehalf()) {
+            jsonGenerator.writeStringField("behalf", dsaResponse.getBehalf());
+        }
+
+        if (dsaResponse.hasPaid()) {
+            jsonGenerator.writeStringField("paid", dsaResponse.getPaid());
+        }
+
+        transparencyJsonWriter.writeTransparencies(dsaResponse.getTransparencyList(), jsonGenerator);
+
+        if (dsaResponse.hasAdrender()) {
+            jsonGenerator.writeBooleanField("adrender", dsaResponse.getAdrender());
+        }
+    }
+}
